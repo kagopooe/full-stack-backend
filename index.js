dca7e28c72ac06e45c62d374ba1941ad2179aed7
kagopooe <kagopooe@gmail.com>
2022-02-23 16:01:41 +0200

    wip: added authetication

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const dotenv = require('dotenv')
 const mongoose = require('mongoose');
 //importing routes
 const authRoute = require('./routes/auth')
+const postRoute = require('./routes/posts');
+const usersRoute = require('./routes/users')
+const res = require('express/lib/response');
 
 dotenv.config();
 
@@ -16,7 +19,15 @@ mongoose.connect(process.env.DB_CONNECT, () => console.log('Connected to db')
 //middleware
 app.use(express.json());
 //route middleware
+app.get('/',(req,res) => {
+    res.send({msg: "List of routes:"})
+})
+
+// app.get('/api:id', (req,res) )
+
 app.use('/api/user', authRoute)
+app.use('/api/posts', postRoute)
+app.use('/api/users', usersRoute)
 
 
 
---
+++ routes/posts.js
@@ -0,0 +1,13 @@
+const res = require('express/lib/response');
+const router = require('express').Router();
+const verify = require('./verifyToken');
+
+router.get('/', verify, (req,res) => {
+    res.send(req.user)
+
+});
+
+
+
+
+module.exports = router;
