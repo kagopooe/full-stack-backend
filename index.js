2e02068b04de9d25924457fe322f69d65ff7bc9a
kagopooe <kagopooe@gmail.com>
2022-02-23 09:50:20 +0200

    WIP: create register

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express = require('express')
 const app = express();
-
+const dotenv = require('dotenv')
+const mongoose = require('mongoose');
 //importing routes
 const authRoute = require('./routes/auth')
+
+dotenv.config();
+
+
+
+//connect to db
+mongoose.connect(process.env.DB_CONNECT, () => console.log('Connected to db')
+);
+
+//middleware
+app.use(express.json());
+//route middleware
+app.use('/api/user', authRoute)
+
 
 
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,20 @@
 const res = require('express/lib/response');
-
 const router = require('express').Router();
+const User = require('../model/User');
 
-router.post('/register', (req,res) => {
-    res.send('Register')
-})
+router.post('/register', async (req,res) => {
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
+    try {
+        //const savedUser = 
+
+    }catch(err) {
+        res.status(400).send(err);
+    }
+});
 
 
 
