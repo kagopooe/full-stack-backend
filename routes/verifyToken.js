88c029d6f08a081e6423aaf4cdf3509f74f498d1
kagopooe <kagopooe@gmail.com>
2022-03-03 16:01:49 +0200

    Update

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -2,16 +2,18 @@ const jwt = require("jsonwebtoken");
 
 module.exports = function (req, res, next) {
   const header = req.headers["authorization"];
-  if (!header) return res.status(401).send({message: "Access Denied"});
+  console.log(req.headers);
+  if (!header) return res.status(401).send({ message: "Access Denied" });
 
   try {
     const bearer = header.split(" ");
+    console.log(bearer);
     const token = bearer[1];
     const verified = jwt.verify(token, process.env.TOKEN_SECRET);
     req.user = verified;
     req.decoded = verified;
     next();
   } catch (err) {
-    res.status(400).send({message: "Invalid Token"});
+    res.status(400).send({ message: "Invalid Token" });
   }
 };
